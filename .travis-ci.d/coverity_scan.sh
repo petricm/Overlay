96a7ca7bd28a96a6b55d08f7de2916bf0e5b8492
Marko Petric <marko.petric@cern.ch>
2017-03-03 12:45:13 +0100

    use CC7 compiler and add fPIC

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -3,10 +3,13 @@
 ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_gcc
 source $ILCSOFT/init_ilcsoft.sh
 
+source /cvmfs/clicdp.cern.ch/compilers/gcc/6.2.0/x86_64-centos7/setup.sh
+source /cvmfs/clicdp.cern.ch/software/CMake/3.6.2/x86_64-centos7-gcc62-opt/setup.sh
+
 cd /Package
 mkdir build
 cd build
-cmake -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+cmake -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fPIC" .. && \
 export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 cov-build --dir cov-int make -j2 && \
 tar czvf myproject.tgz cov-int
