e77db9f213af8043e413acf983b260155af21c59
Marko Petric <marko.petric@cern.ch>
2017-03-03 11:15:37 +0100

    Propper cmake falgs

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake .. && \
+cmake -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
 export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 cov-build --dir cov-int make -j2 && \
 tar czvf myproject.tgz cov-int
