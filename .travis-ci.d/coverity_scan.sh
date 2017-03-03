36410adfec559c5b372a54061c150c714d3d47e4
Marko Petric <marko.petric@cern.ch>
2017-03-03 13:25:34 +0100

    Debug

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_llvm
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_gcc
 source $ILCSOFT/init_ilcsoft.sh
 
-source /cvmfs/clicdp.cern.ch/compilers/llvm/3.9.0/x86_64-centos7/setup.sh
-source /cvmfs/clicdp.cern.ch/software/CMake/3.6.2/x86_64-centos7-llvm39-opt/setup.sh
+source /cvmfs/clicdp.cern.ch/compilers/gcc/6.2.0/x86_64-centos7/setup.sh
+source /cvmfs/clicdp.cern.ch/software/CMake/3.6.2/x86_64-centos7-gcc62-opt/setup.sh
 
 cd /Package
 mkdir build
 cd build
 cmake -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fPIC" .. && \
 export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
-cov-build --dir cov-int make -j2 && \
+cov-build --dir cov-int make VERBOSE=1 -j2 && \
 tar czvf myproject.tgz cov-int
